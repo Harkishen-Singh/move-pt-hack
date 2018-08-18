388aea3a728162a588dd88f3eddae4756588418d
Harkishen-Singh <harkishensingh@hotmail.com>
2018-08-18 13:41:50 +0530

    adding sidebar layouts

--- front-angular/app.js
+++ front-angular/app.js
@@ -1,13 +1,34 @@
 var app = angular.module('pt_management', ['ngRoute']);
 
 app.config(function($routeProvider,$locationProvider) {
-    $routeProvider.when('/',{
+    $routeProvider
+    .when('/',{
         templateUrl:'./html_components/login.html',
         controller:'loginController',
         title:'Login | SignUp',
     })
+    .when('/dashboard', {
+        templateUrl:'./html_components/dashboard.html',
+        controller:'dashController',
+        title:'Dashboard',
+    })
 })
 
-app.controller('loginController', function($scope,$routeProvider,$location) {
+app.controller('loginController', function($scope,$location) {
     console.warn('login page called')
+    $scope.showHeader = false;
+    $scope.title = 'Login | SignUP'
+    $scope.wrongpass = '';
+    $scope.checkLogin =  function() {
+        if($scope.password=='1') {
+            console.warn('logged in')
+            $scope.wrongpass = 'Success';
+            $location.path('/dashboard');
+        }
+        else {
+            $scope.wrongpass = 'Wrong Password entered'
+        }
+    }
+
+    
 })
